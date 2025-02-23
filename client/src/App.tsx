bfc053f5649ca68b2b570442cf22725fd3da71e4
SenseiGlitch <SenseiGlitch@outlook.com>
2025-02-23 23:21:52 +0200

    Fix routing issue for Vercel deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -15,7 +15,7 @@ import Waits from "@/pages/modules/waits";
 import MultiWindow from "@/pages/modules/multi-window";
 import EndToEnd from "@/pages/modules/end-to-end";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -36,10 +36,12 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toaster />
-    </QueryClientProvider>
+    <Router base="/">
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+        <Toaster />
+      </QueryClientProvider>
+    </Router>
   );
 }
 
